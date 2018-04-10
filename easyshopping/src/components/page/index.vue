408c83e885be8acb30502a0f921f8986e6058f0e
monkeykg <923192905@qq.com>
2018-04-10 13:32:05 +0800

    基本页面结构

--- easyshopping/src/components/page/index.vue
+++ easyshopping/src/components/page/index.vue
@@ -1,17 +1,22 @@
 <template>
     <div>
         <Header></Header>
-        <div>main</div>
+        <div >
+            <list></list>
+        </div>
     </div>
    
 </template>
 <script>
 import Header from '@/components/common/header.vue'
+import list from '@/components/page/list.vue'
+
+
 export default {
   name:'index',
   components:{
       Header,
-    
+        list
   }
 }
 </script>
---
+++ easyshopping/src/components/page/list.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-tabs v-model="activeName" >
+    <el-tab-pane label="全部" name="first">全部</el-tab-pane>
+    <el-tab-pane label="汉堡" name="second">汉堡</el-tab-pane>
+    <el-tab-pane label="薯条" name="third">薯条</el-tab-pane>
+    <el-tab-pane label="炸鸡" name="fourth">炸鸡</el-tab-pane>
+    <el-tab-pane label="饮料" name="fifth">饮料</el-tab-pane>
+    <el-tab-pane label="套餐1" name="sixth">套餐1</el-tab-pane>
+    <el-tab-pane label="套餐2" name="seventh">套餐2</el-tab-pane>
+  </el-tabs>
+</template>
+<script>
+export default {
+  name:'list',
+  data(){
+      return{
+activeName:'first'
+      }
+  }
+  
+}
+</script>
+<style>
+
+</style>
